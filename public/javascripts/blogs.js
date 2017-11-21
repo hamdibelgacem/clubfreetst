2567c29753baf415b91bc2b74471ff128be2a520
hamdibelgacem <belgacem.hamdi@ensi-uma.tn>
2017-11-21 09:45:45 +0100

    final project

--- public/javascripts/blogs.js
+++ public/javascripts/blogs.js
@@ -2,9 +2,16 @@ $(document).ready(function () {
 
     var baseUrl = "https://clubfreetst.herokuapp.com/";
 
-    $("#blogID").val();
+
+
     $("#search").click(function (e) {
+
         e.preventDefault();
+
+        $("#result").css({
+            display: "none",
+        });
+
         var $blogID = $("#blogID").val();
         if($blogID.length >= 5 && $blogID.length <= 7){
             /**
@@ -13,20 +20,26 @@ $(document).ready(function () {
 
             $.get(baseUrl+"blogs/"+$blogID).done(function (data) {
                 console.log(data);
-                $("#result").css({
-                    display: "block",
-                });
 
-                $("#blogger").text(data.blogger);
-                $("#title").text(data.title);
-                var notes = data.notes;
-                var list="";
-                notes.forEach(function (note, i) {
-                    list+=`<li id=${note.id}>
+                if(data.err){
+                   alert(data.err);
+                }else{
+                    $("#result").css({
+                        display: "block",
+                    });
+                    $("#blogger").text(data.blogger);
+                    $("#title").text(data.title);
+                    var notes = data.notes;
+                    var list="";
+                    notes.forEach(function (note, i) {
+                        list+=`<li id=${note.id} class="list-group-item">
                             ${note.title}
                             </li>`;
-                });
-                $("#notes").html(list);
+                    });
+                    $("#notes").html(list);
+                }
+
+
             });
 
         }
